6e977dc32537bb751bd58712764af08622dd70d3
Krishna Prasad <Krishna Prasad>
2016-08-14 22:46:40 -0700

    Added changes to work with Amazon

--- 1startGitLab.sh
+++ 1startGitLab.sh
@@ -5,19 +5,19 @@
 #TODO use docker-compose instead (see doc)
 
 #postgresql
-docker run --name gitlab-postgresql -d \
+sudo docker run --name gitlab-postgresql -d \
     --env 'DB_NAME=gitlabhq_production' \
     --env 'DB_USER=gitlab' --env 'DB_PASS=password' \
     --volume /srv/docker/gitlab/postgresql:/var/lib/postgresql \
     sameersbn/postgresql:9.4-3
 
 #redis
-docker run --name gitlab-redis -d \
+sudo docker run --name gitlab-redis -d \
     --volume /srv/docker/gitlab/redis:/var/lib/redis \
     sameersbn/redis:latest
 
 #gitlab
-docker run --name gitlab -d \
+sudo docker run --name gitlab -d \
     --link gitlab-postgresql:postgresql --link gitlab-redis:redisio \
     --publish 10022:22 --publish 10080:80 \
     --env 'GITLAB_PORT=10080' --env 'GITLAB_SSH_PORT=10022' \
--- hello-world-app-deployment/runDockerContainer.sh
+++ hello-world-app-deployment/runDockerContainer.sh
@@ -1,18 +1,17 @@
 #! /bin/bash
 
-DOCKER_REPO=192.168.35.217:5000/hello-world-app
+DOCKER_REPO=54.175.165.244:5000/hello-world-app
 TAG=0.0.1-SNAPSHOT
 CONTAINER_NAME=hello-world-app-deployment
 
 containerId=`docker ps -qa --filter "name=$CONTAINER_NAME"`
 if [ -n "$containerId" ]
 then
-	echo "Stopping and removing existing hello-world container"
-	docker stop $CONTAINER_NAME
-	docker rm $CONTAINER_NAME
+        echo "Stopping and removing existing hello-world container"
+        docker stop $CONTAINER_NAME
+        docker rm $CONTAINER_NAME
 fi
 
 docker run -d --name $CONTAINER_NAME -p 9090:8080 $DOCKER_REPO:$TAG
 #use -d to run in background
 #use -it to run in foreground and get the output. add --rm to remove the container (container's file system) when it exits.
-
